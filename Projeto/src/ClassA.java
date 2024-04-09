092787d970447c4928a838f93cf47ec0c0693283
Rafaela Nunes <rafaelanunes369@gmail.com>
2024-04-09 15:17:31 -0300

    Adicionando novos arquivos A e B

---
+++ Projeto/src/ClassA.java
@@ -0,0 +1,20 @@
+public class ClassA {
+    private int a1;
+    private float a2;
+
+    public int getA1() {
+        return a1;
+    }
+
+    public float getA2() {
+        return a2;
+    }
+
+    public void ma1(){
+        System.out.println("Metodo MA1");
+    }
+
+    public void ma2(){
+        System.out.println("Metodo MA2");
+    }
+}
